456e40d2e0aed5b65a84b1eee4c72f5b1c11625d
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2023-08-24 16:46:05 +0530

    Working

--- src/Component/mainComponents/Products/test.jsx
+++ src/Component/mainComponents/Products/test.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import Prdimg1 from "../../../assets/img/prod-img/Digesto-Plus.jpg";
+// import React from "react";
+// import Prdimg1 from "../../../assets/img/prod-img/Digesto-Plus.jpg";
 
-const prd = () => {
-  const imageSizes = [
+// const prd = () => {
+//   const imageSizes = [
     
-      {name: "digesto"},
-      {price: "07"},
-      {img: "./abtimg1"}
+//       {name: "digesto"},
+//       {price: "07"},
+//       {img: "./abtimg1"}
    
-  ];
-  return (
-    <>
-      <div className="prd">
-        {imageSizes.map((user) => (
-          <div className="user">{user.img}</div>
-        ))}
-      </div>
-    </>
-  );
-};
+//   ];
+//   return (
+//     <>
+//       <div className="prd">
+//         {imageSizes.map((user) => (
+//           <div className="user">{user.img}</div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
 
-export default prd;
+// export default prd;
