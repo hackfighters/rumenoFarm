87c8e7916724724ac1be5ddc3ff4d3ce5b9a83e9
razikkhan98 <razik.khan@skillintelligence.tech>
2023-09-27 00:53:53 +0530

    workig

--- src/Component/Common/Navbar/index.jsx
+++ src/Component/Common/Navbar/index.jsx
@@ -20,7 +20,7 @@ import i18next from "i18next";
 
 // Common Component
 import Login from "../../Common/Modal/Login";
-import Registration  from "../../Common/Modal/Registion";
+import Registration from "../../Common/Modal/Registion";
 
 import Select from "../../Common/Select/index";
 
@@ -28,7 +28,6 @@ import Select from "../../Common/Select/index";
 import logo from "../../../assets/img/lv-bgr.png";
 
 const Navbar = ({ cart, count }) => {
-  
   // State
   const [showlogin, setshowlogin] = useState(false);
   const [lgShow, setLgShow] = useState(false);
@@ -37,7 +36,6 @@ const Navbar = ({ cart, count }) => {
   const [selectedOption, setSelectedOption] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [showRegistrationModal, setShowRegistrtionModal] = useState(false);
-
 
   const username = "admin";
   const password = "password";
@@ -70,13 +68,13 @@ const Navbar = ({ cart, count }) => {
   };
 
   const openRegistration = () => {
-    setShowRegistrtionModal(true)
+    setShowRegistrtionModal(true);
     setShowModal(false);
-  }
+  };
 
   const closeRegistrationModal = () => {
-    setShowRegistrtionModal(false)
-  }
+    setShowRegistrtionModal(false);
+  };
 
   useEffect(() => {
     setCART(cart);
@@ -89,7 +87,7 @@ const Navbar = ({ cart, count }) => {
           <div id="topbar" className="col-sm-12 d-flex align-items-center ">
             <div className="container-fluid">
               <div className="contact-info row justify-content-center">
-                <div className="col-lg-3 d-flex cnt align-items-center">
+                <div className="col-sm-3 d-flex cnt align-items-center">
                   <FontAwesomeIcon
                     icon={faLocationDot}
                     className="me-2"
@@ -100,7 +98,7 @@ const Navbar = ({ cart, count }) => {
                     Pradesh 226002
                   </span>
                 </div>
-                <div className="col-lg-3 d-flex cnt justify-content-center align-items-center">
+                <div className="col-sm-2 d-flex cnt justify-content-center align-items-center">
                   <FontAwesomeIcon
                     icon={faEnvelope}
                     className="me-2"
@@ -108,7 +106,7 @@ const Navbar = ({ cart, count }) => {
                   />
                   <span className="text-dark">rumeno.farmotech@gmail.com</span>
                 </div>
-                <div className="col-lg-2 d-flex cnt justify-content-end align-items-center">
+                <div className="col-sm-2 d-flex cnt justify-content-end align-items-center">
                   <FontAwesomeIcon
                     icon={faClock}
                     className="me-2"
@@ -116,7 +114,7 @@ const Navbar = ({ cart, count }) => {
                   />
                   <span className="text-dark  ">Time 9:00am - 8:00pm</span>
                 </div>
-                <div className="col-lg-2 d-flex cnt align-items-center justify-content-center">
+                <div className="col-sm-2 d-flex cnt align-items-center justify-content-center">
                   <FontAwesomeIcon
                     icon={faPhone}
                     className="me-2"
@@ -124,7 +122,7 @@ const Navbar = ({ cart, count }) => {
                   />
                   <span className="text-dark  ">+91 7355043892</span>
                 </div>
-                <div className="col-lg-2 align-items-center  nav-lang-switch">
+                <div className="col-sm-2 align-items-center  nav-lang-switch">
                   <FontAwesomeIcon
                     type="button"
                     onClick={toggleSelect}
@@ -219,22 +217,27 @@ const Navbar = ({ cart, count }) => {
                     <span className="badge-cart">{count}</span>
                   </Link>
                 </li>
-                <li className="nav-item" id="cart">
-                  <button
-                    className="custom-btn btn-11 border-0 my-2"   
-                    type="button"
-                    onClick={openModal}
-                  >
-                    Login<div class="dot"></div>
-                  </button>
-                  <Login showModal={showModal} closeModal={closeModal} openRegistrationModal={openRegistration}/>
-                  <Registration showModal={showRegistrationModal} closeModal={closeRegistrationModal} />
+                <li className="nav-item logo-width logo-width" id="cart">
+                  <div className="d-flex justify-content-center">
+                  <button className="btn text-white border-0 gradient-custom-2 my-2 w-100" onClick={openModal}>Login</button>
+                    {/* <ToastContainer /> */}
+                  </div>
+
+                  <Login
+                    showModal={showModal}
+                    closeModal={closeModal}
+                    openRegistrationModal={openRegistration}
+                  />
+                  <Registration
+                    showModal={showRegistrationModal}
+                    closeModal={closeRegistrationModal}
+                  />
                 </li>
                 {showlogin ? (
                   <li>
                     <h4
                       typeof="button"
-                      className="text-danger bg-light m-0 p-1 rounded-circle my-2"
+                      className="text-danger bg-light m-0 p-1 rounded-circle"
                     >
                       HV
                     </h4>
