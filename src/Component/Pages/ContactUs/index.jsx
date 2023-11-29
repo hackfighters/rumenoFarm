e2feda0228d0a190a69fb102b1caf554479d5bb2
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2023-11-28 20:46:30 -0800

    api-on-all-form-with-useform

--- src/Component/Pages/ContactUs/index.jsx
+++ src/Component/Pages/ContactUs/index.jsx
@@ -1,68 +1,229 @@
+// import React from "react";
+
+// // Third party Fortawesome
+// import {
+//   faFacebook,
+//   faInstagram,
+//   faWhatsapp,
+// } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faEnvelope,
+//   faLocationDot,
+//   faPhone,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// // Third party i18next
+// import { useTranslation } from "react-i18next";
+
+// // Common Component
+// import Footer from "../../Common/Footer";
+// import MapComp from "../../Common/Map/map";
+// import Navbar from "../../Common/Navbar/index";
+// import ResponsiveNavbar from "../../Common/Navbar/navMob";
+// import { Form } from "react-bootstrap";
+// // {/* Rumeno farm  */}
+// // {/* Rumeno */}
+// // {/* Veterinary */}
+// const ContactUs = () => {
+//   const { t } = useTranslation();
+
+//   return (
+//     <>
+//        <div className="desk-nav">
+//     <Navbar/>
+//     </div>
+//     <div className="mob-nav">
+// <ResponsiveNavbar/>
+//     </div>
+//       <section className="contact-sect container-fluid">
+//         <div className="row justify-content-center py-5">
+//           <div className="col-sm-10">
+//             <div className="contact_inner">
+//               <div className="row justify-content-between">
+//                 <div className="col-sm-8">
+//                   <div className="contact_form_inner row  justify-content-center">
+//                     <Form className="contact_field col-sm-7 pt-1 pb-3">
+//                       <h3 className="my-2 text-center">Contact Us</h3>
+//                       <p className="my-3 text-center">
+//                         Feel Free to contact us any time.
+//                       </p>
+//                       <input
+//                         type="text"
+//                         className="form-control form-group py-3"
+//                         placeholder="Name"
+//                       />
+//                       <input
+//                         type="text"
+//                         className="form-control form-group py-3"
+//                         placeholder="Email"
+//                       />
+//                       <textarea
+//                         className="form-control form-group py-3"
+//                         placeholder="Message"
+//                       ></textarea>
+//                       <button className="contact_form_submit mt-5">Send</button>
+//                     </Form>
+//                   </div>
+//                 </div>
+//                 <div className="col-md-2">
+//                   <div className="right_conatct_social_icon d-flex align-items-end justify-content-center">
+//                     <div className="socil_item_inner d-flex py-2">
+//                       <FontAwesomeIcon
+//                         className="text-white mx-3 h3"
+//                         type="button"
+//                         icon={faFacebook}
+//                       />
+//                       <FontAwesomeIcon
+//                         className="text-white mx-3 h3"
+//                         type="button"
+//                         icon={faInstagram}
+//                       />
+//                       <FontAwesomeIcon
+//                         className="text-white mx-3 h3"
+//                         type="button"
+//                         icon={faWhatsapp}
+//                       />
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="contact_info_sec">
+//                 <h4 className="text-white py-2">Contact Info</h4>
+//                 <div className="d-flex info_single align-items-center my-4">
+//                   <FontAwesomeIcon
+//                     className="text-white mx-2 "
+//                     type="button"
+//                     icon={faPhone}
+//                   />
+//                   <span className="text-white">+91 7355043892</span>
+//                 </div>
+//                 <div className="d-flex info_single align-items-center my-4">
+//                   <FontAwesomeIcon
+//                     className="text-white mx-2 "
+//                     type="button"
+//                     icon={faEnvelope}
+//                   />
+//                   <span className="text-white">rumeno.farmotech@gmail.com</span>
+//                 </div>
+//                 {/* Rumeno farm  */}
+//                 {/* Rumeno */}
+//                 {/* Veterinary */}
+//                 <div className="d-flex info_single align-items-center my-4">
+//                   <FontAwesomeIcon
+//                     className="text-white mx-2 "
+//                     type="button"
+//                     icon={faLocationDot}
+//                   />
+//                   <span className="text-white">{t("v301")}</span>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//       <div className="container-fluid ">
+//         <div className="row justify-content-center">
+//           <div className="col-sm-12">
+//             <div className="map_inner mb-4">
+//               <MapComp />
+//             </div>
+//           </div>
+//         </div>
+//         {/* Rumeno farm  */}
+//         {/* Rumeno */}
+//         {/* Veterinary */}
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default ContactUs;
+
+
+
+
 import React from "react";
-
-// Third party Fortawesome
-import {
-  faFacebook,
-  faInstagram,
-  faWhatsapp,
-} from "@fortawesome/free-brands-svg-icons";
+import { useForm } from "react-hook-form";
+import { Form } from "react-bootstrap";
+import { faFacebook, faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faEnvelope,
-  faLocationDot,
-  faPhone,
-} from "@fortawesome/free-solid-svg-icons";
-
-// Third party i18next
+import { faEnvelope, faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
-
-// Common Component
 import Footer from "../../Common/Footer";
 import MapComp from "../../Common/Map/map";
 import Navbar from "../../Common/Navbar/index";
 import ResponsiveNavbar from "../../Common/Navbar/navMob";
-// {/* Rumeno farm  */}
-// {/* Rumeno */}
-// {/* Veterinary */}
+import axios from "axios";
+
 const ContactUs = () => {
   const { t } = useTranslation();
+  const { handleSubmit, register, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      // Make a POST request to your API endpoint
+      const response = await axios.post("http://127.0.0.1:5000/api/post_data", data);
+      console.log(response.data); // Handle the response as needed
+    } catch (error) {
+      console.error("Error submitting contact details:", error);
+
+      // Handle error, show a message to the user, etc.
+    }
+  };
 
   return (
     <>
-       <div className="desk-nav">
-    <Navbar/>
-    </div>
-    <div className="mob-nav">
-<ResponsiveNavbar/>
-    </div>
+      <div className="desk-nav">
+        <Navbar />
+      </div>
+      <div className="mob-nav">
+        <ResponsiveNavbar />
+      </div>
       <section className="contact-sect container-fluid">
         <div className="row justify-content-center py-5">
           <div className="col-sm-10">
             <div className="contact_inner">
               <div className="row justify-content-between">
                 <div className="col-sm-8">
-                  <div className="contact_form_inner row  justify-content-center">
-                    <div className="contact_field col-sm-7 pt-1 pb-3">
+                  <div className="contact_form_inner row justify-content-center">
+                    <Form className="contact_field col-sm-7 pt-1 pb-3" onSubmit={handleSubmit(onSubmit)}>
                       <h3 className="my-2 text-center">Contact Us</h3>
-                      <p className="my-3 text-center">
-                        Feel Free to contact us any time.
-                      </p>
-                      <input
-                        type="text"
-                        className="form-control form-group py-3"
-                        placeholder="Name"
-                      />
-                      <input
-                        type="text"
-                        className="form-control form-group py-3"
-                        placeholder="Email"
-                      />
-                      <textarea
-                        className="form-control form-group py-3"
-                        placeholder="Message"
-                      ></textarea>
-                      <button className="contact_form_submit mt-5">Send</button>
-                    </div>
+                      <p className="my-3 text-center">Feel Free to contact us any time.</p>
+                      <Form.Group controlId="formName">
+                        <Form.Control
+                          type="text"
+                          placeholder="Name"
+                          {...register("name", { required: "Name is required" })}
+                          isInvalid={errors.name}
+                        />
+                        <Form.Control.Feedback type="invalid">{errors.name && errors.name.message}</Form.Control.Feedback>
+                      </Form.Group>
+
+                      <Form.Group controlId="formEmail">
+                        <Form.Control
+                          type="text"
+                          placeholder="Email"
+                          {...register("email", { required: "Email is required", pattern: { value: /^\S+@\S+$/i, message: "Invalid email address" } })}
+                          isInvalid={errors.email}
+                        />
+                        <Form.Control.Feedback type="invalid">{errors.email && errors.email.message}</Form.Control.Feedback>
+                      </Form.Group>
+
+                      <Form.Group controlId="formMessage">
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          placeholder="Message"
+                          {...register("message", { required: "Message is required" })}
+                          isInvalid={errors.message}
+                        />
+                        <Form.Control.Feedback type="invalid">{errors.message && errors.message.message}</Form.Control.Feedback>
+                      </Form.Group>
+
+                      <button className="contact_form_submit mt-5" type="submit">Send</button>
+                    </Form>
                   </div>
                 </div>
                 <div className="col-md-2">
@@ -121,7 +282,7 @@ const ContactUs = () => {
           </div>
         </div>
       </section>
-      <div className="container-fluid ">
+      <div className="container-fluid">
         <div className="row justify-content-center">
           <div className="col-sm-12">
             <div className="map_inner mb-4">
@@ -129,9 +290,6 @@ const ContactUs = () => {
             </div>
           </div>
         </div>
-        {/* Rumeno farm  */}
-        {/* Rumeno */}
-        {/* Veterinary */}
       </div>
       <Footer />
     </>
